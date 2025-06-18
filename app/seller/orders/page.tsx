02d1b283a8663a81b9c7a9e4fddf1439201a4b6f
Mohamed Tharwat <mthawrat@example.com>
2025-06-18 17:53:54 +0300

    Fix ESLint unused import errors

--- app/seller/orders/page.tsx
+++ app/seller/orders/page.tsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import { assets, orderDummyData } from "@/assets/assets";
 import Image from "next/image";
 import { useAppContext } from "@/context/AppContext";
-import Footer from "@/components/seller/Footer";
 import Loading from "@/components/Loading";
 
 const Orders = () => {
